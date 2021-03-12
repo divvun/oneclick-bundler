b848e125488173bf7a99ee53c3802066c9d7adcc
Zoey Riordan <zoey@dos.cafe>
2021-03-12 10:32:39 +0100

    add kbdi as a bundled item

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -12,6 +12,7 @@ use std::{
 const DIVIDER_UUID: u128 = 0xaae8ea9c35484ee4bf28f1a25a6b3c6c;
 const DOTNET_INSTALLER: &str = "dotnet5-webinst.exe";
 const ONECLICK_INSTALLER: &str = "Divvun.Installer.OneClick.exe";
+const KBDI_UTIL: &str = "kbdi.exe";
 
 fn main() -> Result<()> {
     build()?;
@@ -40,7 +41,7 @@ fn dist() -> Result<()> {
 
     bf.set_file_attr("created", now.to_vec())?;
 
-    let files = [DOTNET_INSTALLER, ONECLICK_INSTALLER];
+    let files = [DOTNET_INSTALLER, ONECLICK_INSTALLER, KBDI_UTIL];
 
     for file in &files {
         let box_path = BoxPath::new(file)?;
